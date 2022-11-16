d344a3f51749d62cccf4676e0c936ea431fa0fce
DelhinRharl <delhinrharl@gmail.com>
2022-11-16 20:31:03 +0000

    Add equation

--- countOddNumbers.js
+++ countOddNumbers.js
@@ -1,9 +1,20 @@
+
+const isOdd = (num) => num % 2 === 1;
+
+const addCounts = (low, high) => {
+    const bothOdd = isOdd(low) && isOdd(high);
+    const oneOdd = isOdd(low) || isOdd(high);
+    if (bothOdd) {
+        return 2;
+    } else if (oneOdd) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 var countOdds = function(low, high) {
-    if (low % 2 == 0 && high % 2==0){
-        return(high-low)/ 2
-    }
-    if (high-low==1){
-        return 1
-    }
-    return (high-low) / 2 + 1
+    const diff = high - low;
+    const counts = (diff + addCounts(low, high)) / 2;
+    return counts;
 };
